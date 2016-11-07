957c5d10fb0c352c85590c42390fb5046e819480
Zarete <thomas@Zarete>
2016-11-07 18:29:19 +0100

    clean up

--- GenBank/myProject.py
+++ GenBank/myProject.py
@@ -4,7 +4,7 @@
 
 import myBio as bio
 
-def clean_size(txt):
+def clean_numbers(txt):
     """ Gene size cleaner
     Cleans the size information provided to the function,
     this function is written by Maucourt Thomas
@@ -15,14 +15,15 @@ def clean_size(txt):
     """
 
     if txt.isdigit():
-        return txt
+        return int(txt)
 
     else:
         nb = ''
-        for char in txt.split('\n')[-1]:
+        for char in txt:
             if char.isdigit():
                 nb += char
-        return nb
+
+        return int(nb)
 
 def readGenBank(filename):
     """GenBank flat file parser
@@ -57,28 +58,35 @@ def readGenBank(filename):
         result['data'] = header[header.index('##Genome-Annotation-Data-START##')+33: header.index('##Genome-Annotation-Data-END##')]
 
     for elem in splitted_header:
+
         if 'DEFINITION' in elem:
-            tmp = elem.strip().split('DEFINITION')
-            result['description'] = tmp[1].strip()
+            result['description'] = elem.strip().split('DEFINITION')[1].strip()
+
         elif '/mol_type' in elem:
-            moltype = elem.split('"')[1].strip()
-            result['gbtype'] = moltype
+            result['gbtype'] = elem.split('"')[1].strip()
 
             if 'RNA' in moltype.upper():
                 otype = 'rna'
+
             elif 'DNA' in moltype.upper():
                 otype = 'dna'
+
             elif 'PROTEIN' in moltype.upper():
                 otype = 'protein'
+
             else:
                 otype = ''
+
             result['type'] = otype
+
         elif 'ACCESSION' in elem:
             ID = elem.split("ACCESSION")[1].strip()
             result['ID'] = ID
+
         elif '/organism' in elem:
             organism = elem.split('"')[1].strip()
             result['organism'] = organism
+
         elif '     source     ' in elem:
             size = elem.split('..')[1].strip()
             result['length'] = size
@@ -92,43 +100,46 @@ def readGenBank(filename):
         dic_result = {'start' : 0,'stop' : 0, 'frame' : 0, 'length' : 0, 'name' : 'unknown', 'protein' : 'xxx', 'product' : 'unknown'}
 
         if "product=" in elem:
-            product = elem.split("/product=")[1].replace(" "*20, "").replace('\n', '').split('"')[1]
-            dic_result['product'] = product
+            dic_result['product'] = elem.split("/product=")[1].replace(" "*20, "").replace('\n', '').split('"')[1]
 
         sub_section = elem.split('\n')[0]
-        if '..' in sub_section and '/' not in sub_section:
+        if '..' in sub_section:
+            start, stop, length = 0, 0, 0
+
             if ',' in sub_section:
-                sub, final = [], []
-                under = sub_section.split(',')
-                for elem in under:
-                    sub.extend(elem.split('..'))
+                tmp, final = [], []
+                for elem in sub_section.split(','):
+                    tmp.extend(elem.split('..'))
 
-                for elem in sub:
-                    final.append(clean_size(elem))
+                for elem in tmp:
+                    final.append(clean_numbers(elem))
 
-                dic_result['start'] = int(final[1])
-                dic_result['stop'] = int(final[3])
-                dic_result['length'] = (int(final[1]) - int(final[0])) + (int(final[3]) - int(final[2]))
+                final = sorted(final)
+
+                start = final[1]
+                stop = final[3]
+                length = (final[1] - final[0]) + (final[3] - final[2])
 
             else:
-                start = clean_size(sub_section.strip().split('..')[0])
-                stop = clean_size(sub_section.strip().split('..')[1])
-                dic_result['start'] = int(start)
-                dic_result['stop'] = int(stop)
-                dic_result['length'] = int(stop) - int(start)
+                start = clean_numbers(sub_section.strip().split('..')[0])
+                stop = clean_numbers(sub_section.strip().split('..')[1])
+                length = stop - start
+
+            dic_result['start'] = start
+            dic_result['stop'] = stop
+            dic_result['length'] = length
 
         section = elem.split('/')
         for part in section:
-
             if 'codon_start=' in part:
                 dic_result['frame'] = part.strip().replace('\n', '').split('=')[1].strip()
 
             elif 'gene=' in part:
                 name = part.split("=")
                 dic_result['name'] = name[1].replace('\n', '').strip()[1:-1]
+                
             elif 'translation=' in part:
-                protein = part.strip().replace("\n", "").split('\"')[1]
-                dic_result['protein'] = protein.replace(' ', '')
+                dic_result['protein'] = part.strip().replace("\n", "").split('\"')[1].replace(' ', '')
 
         result['genes'].append(dic_result)
 
